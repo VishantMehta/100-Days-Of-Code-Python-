89efc05b5ea28511289a5dede9d868215f06dbda
VishantMehta <vishantmehta0@gmail.com>
2023-08-21 16:20:58 +0530

    Variable of Python

---
+++ Day-1/Day-1-Variable/main.py
@@ -0,0 +1,23 @@
+name = "Vansh"
+print(name)
+
+name = "Vishant"
+print(name)
+
+name = input("What is your name?\n")
+length = len(name)
+print(length)
+#Notes                                                                                                     
+
+#Variable is something we call name given to data or  something we might refer later in our code but we can change it later in different lines like first we defined a variable name = "Jack", we can change it later and when it is changed , after wards the line we changed it will work with the changed version unless we change it again in some other line.                                                                                                                                                                         
+#Definition - A variable give a name to a piece of data.Like a box with a label, it tells you what's inside the box.                                                                                                                                                                                                            It is defined by the syntax like - variable = "data"                                                           example - my_name = "Angela" , my_age = 12 etc.                                                                                                                                                              
+#Like in the above code , we defined different variable and used them in our code later , this solves us the problem that before we only get input and that data   once taken is gone after used once but after this we can code them easily aand use many time                                                                                                                                                                  
+#Solve the assignment 4 of day 1 on variable , it was very good , not able to solve that by myself - In that Refer this example which will help to solve that we have two cups one of cofee and one milk and if we have to swith content inside them , what we will do ? - get a new cup which will help us, similarly take another variablee which can be used for switching them .                                                                                                                                                       
+#Variable Naming -:                                                                                                                                                                                                  
+# Now when it comes to naming your variable , you can pretty much call it whatever you want .     Like in above code instead of name we can call it as n = input("What is your name?") and etc                                                                                                                               
+#But there are couple of rules we should follow  The most important one is make our code readable , like if we revisit our code around 6-12 months , the n and l named variable would not make sense , so try to make our code more readable :-                                                                                                                                                                                      
+#Numbers can't be at the beginning of name of our variable we defined like 3length , 665l etc. But we can have numbers in between or at the end like length3 etc.                                                                                                                                                             
+#In naming of variable you can't have a space in between , instead of that in python , we use underscore   ( _ ) to seperate the words like user_name. If you try to run with space it will create a syntax error.                                                                                                              
+#There are certain previlaged words like print, input which are used as funcction should'nt be used as name of variables, even though code will work but it's a bad practice                                                                                                                                                 
+#If we get the error like namerror - variable - ______ is not defined , then we have misspelled the variable at somee point , so we to check that.                                                                                                                                                                        
+#After this do day 1 project  
